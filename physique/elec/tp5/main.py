26d6d44fb567d247d8231febf45492f8ed1223c1
Als0cK <albanvacher76@gmail.com>
2024-11-07 14:00:56 +0100

    add tp elec 5

---
+++ physique/elec/tp5/main.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_csv(filename):
+    data  = pd.read_csv(filename, delimiter=",")
+    
+    time = data["Temps"].to_numpy()
+    bobine = data['EA04_D'].to_numpy()
+    resistor = data['EA15_D'].to_numpy()
+    generator = data['EA26_D'].to_numpy()
+
+    return time, bobine, resistor, generator
+
+
+def find_slice(data, kind):
+    start = 0
+    end = 0
+    i = 0
+    for a, b in zip(data, data[1:]):
+        if np.sign(a) != np.sign(b):
+            if np.sign(a) == kind:
+                start = i
+            else:
+                end = i
+                return start, end, False
+    return start, end, True
+
+def find_best_slice(data, kind):
+    start = 0
+    end = 0
+    
+    while True:
+        new_start, new_end, should_break = find_slice(data[end:], kind)
+        if new_end - new_start > end - start:
+            start = new_start + end
+            end = new_end + end
+        if should_break:
+            break
+    return start, end
+
+
+def main():
+    time, bobine, resistor, generator = read_csv("data.csv")
+
+    start, end = find_best_slice(generator, False)
+
+    print(start, end)
+
+    plt.title("test")
+    plt.xlabel("Temps (s)")
+    plt.ylabel("Tension (V)")
+    plt.grid(True)
+
+    plt.plot(time[start:end], bobine[start:end], 'r+', label='Tension aux bornes de la bobine')
+    plt.plot(time[start:end], generator[start:end], 'b+', label='Tension aux bornes du générateur')
+    plt.plot(time[start:end], resistor[start:end], 'g+', label='Tension aux bornes de la résistance')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
