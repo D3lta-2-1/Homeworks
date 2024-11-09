b6ff65a32d36ddab955f43e410d392626cf4b60c
Etienne Thomas <83219809+D3lta-2-1@users.noreply.github.com>
2024-11-09 19:24:45 +0100

    progress in tp5

--- physique/elec/tp5/main.py
+++ physique/elec/tp5/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools as it
+import scipy.signal as signal
+
+RESISTANCE = 2300 #ohm
 
 def read_csv(filename):
     data  = pd.read_csv(filename, delimiter=",")
@@ -13,66 +16,170 @@ def read_csv(filename):
 
     return time, bobine, resistor, generator
 
-
 def find_slice(data, kind):
     slice = (0, 0)
     start = 0
-    i = 0
-    for a, b in it.pairwise(data):
+    for (a, b), i in zip(it.pairwise(data), it.count()):
         if np.sign(a) != np.sign(b):
             if np.sign(b) == kind:
-                print("find start at", i)
                 start = i
             else:
                 end = i
-                print("find end", i)
                 if slice[1] - slice[0] < end - start:
                     slice = (start, end)
-                    print("new best slice", slice)
-        i += 1
     return slice
 
 def find_slice_and_ignore(data, kind):
     start, end = find_slice(data, kind)
-    return start + 2, end #ignore imprecision at start
+    return start + 5, end #ignore imprecision at start
+
+def smooth(data, window_size=20):
+    return signal.savgol_filter(data, window_size, 2)
+
+def find_value(data, value, is_growing):
+    start = 0
+    end = len(data)
+
+    smaller = lambda a, b: a < b
+    greater = lambda a, b: a > b
+
+    comp1 = smaller if is_growing else greater
+    comp2 = greater if is_growing else smaller
+
+    while True:
+        index = (start + end) // 2
+        if comp1(data[index], value):
+            start = index
+        if comp2(data[index], value):
+            end = index
+        if end - start < 2:
+            return index
+
+def find_at_index(value, percent, is_growing):
+    start = value[0]
+    end = value[-1]
+    target_tau = (1 - percent) * start + percent * end
+    index = find_value(value, target_tau, is_growing)
+    return index
+
+def find_time_at(time, value, percent, is_growing):
+    index = find_at_index(value, percent, is_growing)
+    return time[index] - time[0], time[index], value[index]
+
+def draw(time, bobine, resistor, generator, title):
+    plt.title(title)
+    plt.xlabel("Temps (µs)")
+    plt.ylabel("Tension (V)")
+    plt.grid(True)
+    plt.plot(time, generator, 'b+', label="Générateur")
+    plt.plot(time, resistor, 'g+', label="Résistance")
+    plt.plot(time, bobine, 'r+', label="Bobine")
+
+def draw_raw(time, bobine, resistor, generator):
+    plt.subplot(3, 2, 1)
+    
+    draw(time, bobine, resistor, generator, "Valeur brute")
+    plt.legend(loc="best")
+    plt.tight_layout()
+
+def draw_smoothed_charging(time, bobine, resistor, generator):
+    start, end = find_slice_and_ignore(generator, 1)
+
+    bobine = smooth(bobine[start:end])
+    resistor = smooth(resistor[start:end])
+    time = time[start:end]
+    generator = generator[start:end]
+
+    tau, x, y = find_time_at(time, bobine, 0.63, False)
+    plt.subplot(3, 2, 3)
+    plt.annotate("tau = {:}µs".format(tau), (x, y))
+    draw(time, bobine, resistor, generator, "Mise en tension")
+    plt.tight_layout()
+
+def draw_smoothed_decharging(time, bobine, resistor, generator):
+    start, end = find_slice_and_ignore(generator, -1)
+
+    bobine = smooth(bobine[start:end])
+    resistor = smooth(resistor[start:end])
+    generator = generator[start:end]
+    tau, x, y = find_time_at(time[start:end], bobine, 0.63, True)
+
+    plt.subplot(3, 2, 5)
+    plt.annotate("tau = {:}µs".format(tau), (x, y))
+    draw(time[start:end], bobine, resistor, generator, "Régime libre ")
+    plt.tight_layout()
+
+def monte_carlo(raw_time, raw_data, d, n_sim):
+    array=np.zeros(n_sim)
+    for i in range (n_sim):
+        simulated_error = np.random.uniform(-d, d, len(raw_data))
+        simulated_data = raw_data + simulated_error
+        simulated_data = np.log(np.abs(simulated_data))
+        a, _ = np.polyfit(raw_time, simulated_data, 1)
+        array[i] = -1/a
+    
+    return np.mean(array), np.std(array)
+
+def draw_log(time, bobine, generator):
+    
+    start, end = find_slice_and_ignore(generator, 1)
+
+    bobine = smooth(bobine[start:end])
+    time = time[start:end]
+
+    plt.subplot(3, 2, 2)
+    plt.title("Logarithme des valeurs mesurées")
+    log_data = np.log(bobine)
+    plt.plot(time, log_data, 'r+')
+
+    index = find_at_index(bobine, 0.95, False) #work on 95% of the charging tension, the rest is noise
+    
+    time = time[:index]
+    log_data = log_data[:index]
+    bobine = bobine[:index]
+
+    a, b = np.polyfit(time, log_data, 1)
+    
+    plt.plot(time, a * time + b, 'b-')
+    plt.annotate("1/a = {:}".format(-1/a), (time[-1], log_data[-1]))
+    plt.xlabel("""Temps (µs)\n modelisé en bleu\n (95% de la tension)""")
+    plt.ylabel("Log de la tension")
+    plt.tight_layout()
+
+    start, _ = find_slice(generator, -1)
+    d = abs(generator[start + 3]- generator[start + 2])
+
+    N = 10000
+    tau, u_tau = monte_carlo(time, bobine, d, N)
+
+    L = tau * 1e-3 * RESISTANCE # 6 would to H but we want mH
+
+    plt.figtext(0,0 ,"En uitilisant la méthode de Monte-Carlo avec Δ = {:.3}V, n = {}\ntau = {:.3} ± {:.3}\nL = {:.0} mH ".format(d, N, tau, u_tau, L))
+
+def draw_derivative(time, bobine, resistor, generator):
+    start, end = find_slice_and_ignore(generator, 1)
+    bobine = smooth(bobine[start:end])
+    resistor = smooth(resistor[start:end])
+    time = time[start:end]
+
+    derivative = np.gradient(resistor, time)
+
+    plt.subplot(3, 2, 4)
+    plt.title("dUr/dt / Ub")
+    plt.plot(time, derivative / bobine, 'r+')
+    plt.xlabel("Temps (µs)")
+    plt.tight_layout()
 
 def main():
     time, bobine, resistor, generator = read_csv("data.csv")
 
-    plt.subplot(1, 3, 1)
-
-    plt.title("Valeur brute")
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Tension (V)")
-    plt.grid(True)
-    plt.plot(time, bobine, 'r+', label='Tension aux bornes de la bobine')
-    plt.plot(time, generator, 'b+', label='Tension aux bornes du générateur')
-    plt.plot(time, resistor, 'g+', label='Tension aux bornes de la résistance')
-
-    plt.subplot(1, 3, 2)
+    time = time * 1e6 #convert to µs
     
-    start, end = find_slice_and_ignore(generator, 1)
-    print(start, end)
-    plt.title("Mise en tension")
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Tension (V)")
-    plt.grid(True)
-    plt.plot(time[start:end], bobine[start:end], 'r+', label='Tension aux bornes de la bobine')
-    plt.plot(time[start:end], generator[start:end], 'b+', label='Tension aux bornes du générateur')
-    plt.plot(time[start:end], resistor[start:end], 'g+', label='Tension aux bornes de la résistance')
-
-    plt.subplot(1, 3, 3)
-
-    start, end = find_slice_and_ignore(generator, -1)
-    print(start, end)
-    plt.title("Régime libre")
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Tension (V)")
-    plt.grid(True)
-    plt.plot(time[start:end], bobine[start:end], 'r+', label='Tension aux bornes de la bobine')
-    plt.plot(time[start:end], generator[start:end], 'b+', label='Tension aux bornes du générateur')
-    plt.plot(time[start:end], resistor[start:end], 'g+', label='Tension aux bornes de la résistance')
-
+    draw_raw(time, bobine, resistor, generator)
+    draw_smoothed_charging(time, bobine, resistor, generator)
+    draw_smoothed_decharging(time, bobine, resistor, generator)
+    draw_log(time, bobine, generator)
+    draw_derivative(time, bobine, resistor, generator)
 
     plt.show()
 
